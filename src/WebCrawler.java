e9d4c86b66577440af96263d7136934b0378ec7f
jiaonizhou <zhoujiaoni14@gmail.com>
2016-04-10 20:27:31 -0700

    jiaoni's 2nd

--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -1,11 +1,37 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 public class WebCrawler {
+	
+	public static ArrayList<String> findURL(String seed, String domain) throws IOException {
+		Document doc = Jsoup.connect(seed).get();
+		// System.out.println(doc);
+		Elements links = doc.select("a[href]");
+		ArrayList<String> frontier = new ArrayList<String>();
+		
+		for (Element link: links) {
+			String absHref = link.attr("abs:href");
+			if (domain != null) {
+				if(link.attr("abs:href").contains(domain)) {
+					frontier.add(absHref);
+				}
+	    	} else {
+	    		frontier.add(absHref);
+	    	}
+	    }
+		return frontier;
+	}
+	
 	public static void main(String args[]) throws IOException {
 		FileReader in = new FileReader("specification.csv");
 		BufferedReader br = new BufferedReader(in);
@@ -24,11 +50,37 @@ public class WebCrawler {
 		in.close();
 		// System.out.println(seed);
 		// System.out.println(max);
-		// System.out.println(domain);
-		
-		Document doc = Jsoup.connect(seed).get();
-		// System.out.println(doc);
+		// System.out.println(domain); 
+		ArrayList<String> frontier = findURL(seed, domain);
+		int size = frontier.size();
+		FileWriter fw = new FileWriter("report.html");
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write("<!DOCTYPE html>\n");
+		bw.write("\t<head>\n");
+		bw.write("\t\t<title>Web Crawler Report</title>\n");
+		bw.write("\t\t<meta charset='utf-8' />\n");
+		bw.write("\t</head>\n");
+		bw.write("\t<body style='text-align:center; font-family:Sans-serif;'>\n");
+		bw.write("\t\t<h1>URL</h1>\n");
+		for (String url: frontier) {
+			bw.write("\t\t<p>" + url + "</p>\n");
+		}
+		bw.write("\t</body>\n");
+		bw.write("<html>\n");
+		bw.close();
 		
 		
 	}
+	
+	
+
 }
+
+	
+	    
+	    
+	    
+	    
+	    
+	    
+	    

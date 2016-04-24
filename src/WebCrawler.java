7825a4e36b681b23106d0565b2247db3f2fe5dd9
jiaonizhou <zhoujiaoni14@gmail.com>
2016-04-24 15:00:38 -0700

    bfs

--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -8,8 +8,11 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import org.jsoup.Connection;
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -17,76 +20,93 @@ import org.jsoup.select.Elements;
 
 
 public class WebCrawler {
-	public static ArrayList<String> frontier = new ArrayList<String>();
+	public static Queue<String> frontier = new LinkedList<String>();
+	public static ArrayList<String> visited = new ArrayList<String>();
 	public static HashMap<String, Integer> hpCount = new HashMap<String, Integer>();
 	public static HashMap<String, String> hpTitle = new HashMap<String, String>();
 	public static HashMap<String, Integer> hpImage = new HashMap<String, Integer>();
 	public static HashMap<String, Integer> hpLink = new HashMap<String, Integer>();
 	public static HashMap<String, Integer> hpResponse = new HashMap<String, Integer>();
+	public static String UserAgent = 
+			"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
 	public static int count = 0;
 	
 	public static void findURL(String seed, int max, String domain) throws IOException {
-		try {
-			// connect seed, extract title, increase count, put seed into frontier
-			Document doc = Jsoup.connect(seed).get();
-			String title = doc.title();
-			count ++;
-			frontier.add(seed);
-			hpCount.put(seed, count);
-			hpTitle.put(seed, title);
-			
-			// save HTML to repository folder
-			URL url;
-			BufferedReader in;
-			String inputLine;
-			String filename;
-			FileWriter out;
-			BufferedWriter bw;
-			filename = "repository/" + count + ".html";
-			out = new FileWriter(filename);
-			bw = new BufferedWriter(out);
-			url = new URL(seed);
-			in = new BufferedReader(new InputStreamReader(url.openStream()));
-			while ((inputLine = in.readLine()) != null) {
-				bw.write(inputLine);
-			}
-			bw.close();
-			in.close();
-			
-			// get response code
-			Connection.Response response = Jsoup.connect(seed).execute();
-			int responseCode = response.statusCode();
-			hpResponse.put(seed, responseCode);
-			
-			// get image count
-			Elements images = doc.getElementsByTag("img");
-			int imgCount = images.size();
-			hpImage.put(seed, imgCount);
-			
-			// get link count
-			Elements links = doc.select("a[href]");
-			int linkCount = links.size();
-			hpLink.put(seed, linkCount);
-			
-			// spread the crawler
-			for (Element link: links) {
-				if (frontier.size() >= max) {
-					return;
-				}
-				String absHref = link.attr("abs:href");
-				if (domain != null) {
-					if(link.attr("abs:href").contains(domain)) {
-						if (!frontier.contains(absHref)) {
-							findURL(absHref, max, domain);
-						}
+		frontier.add(seed);
+		while (visited.size() < max && !frontier.isEmpty()) {
+			try {
+				String curr = frontier.poll();
+				System.out.println(curr);
+				
+				// get response code
+				Connection.Response response = Jsoup.connect(curr).userAgent(UserAgent).timeout(10000).execute();
+				int responseCode = response.statusCode();
+				hpResponse.put(curr, responseCode);
+				
+				if (responseCode == 200) {
+					// connect current, extract title, increase count, put current into visited
+					Document doc = Jsoup.connect(curr).userAgent(UserAgent).timeout(10000).get();
+					String title = doc.title();
+					count ++;
+					visited.add(curr);
+					hpCount.put(curr, count);
+					hpTitle.put(curr, title);
+					
+					// save HTML to repository folder
+					URL url;
+					BufferedReader in;
+					String inputLine;
+					String filename;
+					FileWriter out;
+					BufferedWriter bw;
+					filename = "repository/" + count + ".html";
+					out = new FileWriter(filename);
+					bw = new BufferedWriter(out);
+					url = new URL(curr);
+					in = new BufferedReader(new InputStreamReader(url.openStream()));
+					while ((inputLine = in.readLine()) != null) {
+						bw.write(inputLine);
 					}
-		    	} else {
-		    		if (!frontier.contains(absHref)) {
-		    			findURL(absHref, max, domain);
-		    		}
-		    	}
+					bw.close();
+					in.close();
+					
+					// get image count
+					Elements images = doc.getElementsByTag("img");
+					int imgCount = images.size();
+					hpImage.put(curr, imgCount);
+					
+					// get link count
+					Elements links = doc.select("a[href]");
+					int linkCount = links.size();
+					hpLink.put(curr, linkCount);
+					
+					// save crawled links to frontier
+					for (Element link: links) {
+						String absHref = link.attr("abs:href");
+						if (domain != null) {
+							if(link.attr("abs:href").contains(domain)) {
+								if (!visited.contains(absHref)) {
+									frontier.add(absHref);
+								}
+							}
+				    	} else {
+				    		if (!visited.contains(absHref)) {
+				    			frontier.add(absHref);
+				    		}
+				    	}
+				    }
+				}
+			} 
+			catch (NullPointerException e) {
+		        e.printStackTrace();
 		    }
-		} catch (IOException e) {
+			
+			catch (HttpStatusException e) {
+				e.printStackTrace();
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
@@ -133,7 +153,7 @@ public class WebCrawler {
 		bw.write("\t\t\t\t<th>number of outlinks</th>\n");
 		bw.write("\t\t\t\t<th>number of images</th>\n");
 		bw.write("\t\t\t</tr>\n");
-		for (String url: frontier) {
+		for (String url: visited) {
 			bw.write("\t\t\t<tr>\n");
 			bw.write("\t\t\t\t<td><a href='" + url + "' target='_blank'>" + hpTitle.get(url) + "</a></td>\n");
 			bw.write("\t\t\t\t<td><a href='repository/" + hpCount.get(url) + ".html' download>" + hpCount.get(url) + ".html</a></td>\n");
